46f3ecee2828407e1955c9e646654ff7f68710ed
Sander in 't Hout <sander@inthout.eu>
2018-03-12 03:30:40 +0100

    use mutex to make sure the user can't start a second instance

--- BrightLight.WPF/App.xaml.cs
+++ BrightLight.WPF/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using BrightLight.Shared.ViewModels;
@@ -17,8 +18,23 @@ namespace BrightLight.WPF
     {
         private HotKey _showHotKey; // TODO: config window to make this a custom set hotkey
 
+        private Mutex _mutex;
+
         private App()
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            try
+            {
+                _mutex = new Mutex(false, "h3x4d3c1m4l BrightLight");
+            }
+            catch
+            {
+                // app already running
+                MessageBox.Show("BrightLight is already running!", "BrightLight - Already running", MessageBoxButton.OK, MessageBoxImage.Information);
+                Environment.Exit(0);
+            }
+
             // https://github.com/cefsharp/CefSharp/issues/1714
             AppDomain.CurrentDomain.AssemblyResolve += Resolver;
 
@@ -34,6 +50,12 @@ namespace BrightLight.WPF
             InitializeCefSharp();
         }
 
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            GC.KeepAlive(_mutex); // don't GC our mutex
+            var ex = (Exception)e.ExceptionObject;
+            // TODO: logging
+        }
 
         [MethodImpl(MethodImplOptions.NoInlining)]
         private static void InitializeCefSharp()
